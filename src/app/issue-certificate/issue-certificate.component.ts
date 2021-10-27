24f063a1a0249afe0d22e2a19d31a02a725ea8b2
Paras <paraspatel1434@gmail.com>
2021-10-27 09:26:19 +0530

    issuer backend added

--- src/app/issue-certificate/issue-certificate.component.ts
+++ src/app/issue-certificate/issue-certificate.component.ts
@@ -5,6 +5,7 @@ import { FormlyJsonschema } from '@ngx-formly/core/json-schema';
 import { JSONSchema7 } from "json-schema";
 import { GeneralService } from '../services/general/general.service';
 import { of as observableOf } from 'rxjs';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-issue-certificate',
@@ -17,7 +18,9 @@ export class IssueCertificateComponent implements OnInit {
     "type": "object",
     "required": [
       "student",
-      "course"
+      "course",
+      "type",
+      "date"
     ],
     "properties": {
       "student": {
@@ -53,18 +56,26 @@ export class IssueCertificateComponent implements OnInit {
   options: FormlyFormOptions;
   fields: FormlyFieldConfig[];
   searchResult: any[];
+  formData = {};
+  requestOptions = {};
+  isAuthenticated = false;
+  accessToken;
+  did;
+  signedCredential: any;
+  generated: boolean=false;
+  qrCode: any;
   constructor(private formlyJsonschema: FormlyJsonschema, public generalService: GeneralService) { }
 
   ngOnInit(): void {
     this.schema.properties.student['widget'] = {
       "formlyConfig": {
-      "templateOptions": {
-      },
-      "validation": {},
-      "expressionProperties": {},
-      "modelOptions": {}
+        "templateOptions": {
+        },
+        "validation": {},
+        "expressionProperties": {},
+        "modelOptions": {}
+      }
     }
-  }
     this.schema.properties.student['widget']['formlyConfig']['type'] = "autocomplete";
     this.schema.properties.student['widget']['formlyConfig']['templateOptions']['placeholder'] = "Student";
     this.schema.properties.student['widget']['formlyConfig']['templateOptions']['label'] = "identityDetails.fullName";
@@ -74,11 +85,11 @@ export class IssueCertificateComponent implements OnInit {
         var formData = {
           "filters": {
             "identityDetails.fullName": {
-                "contains": term
+              "contains": term
             }
-        },
-        "limit": 20,
-        "offset": 0
+          },
+          "limit": 20,
+          "offset": 0
         }
         // formData.filters[field.key] = {};
         // formData.filters[field.key]["contains"] = term
@@ -96,20 +107,254 @@ export class IssueCertificateComponent implements OnInit {
     }
     this.schema.properties.date['widget'] = {
       "formlyConfig": {
-      "templateOptions": {
+        "templateOptions": {
+        }
       }
     }
-  }
     this.schema.properties.date['widget']['formlyConfig']['templateOptions']['type'] = 'date';
     this.form2 = new FormGroup({});
     this.options = {};
     this.fields = [this.formlyJsonschema.toFieldConfig(this.schema)];
+
+    // this.getData();
   }
 
   submit() {
-    alert(JSON.stringify(this.model));
+    console.log(this.model);
+    this.signUp()
   }
 
- 
-  
+  signUp() {
+    this.formData = {
+      "username": (this.model['student']).replace(" ", "").replace(" ", ""),
+      "password": "Test@1234",
+      "options": {
+        "didMethod": "elem",
+        "keyTypes": [
+          "rsa"
+        ]
+      },
+      "messageParameters": {
+        "message": "Hello message",
+        "subject": "test subject",
+        "htmlMessage": "test html message"
+      }
+    }
+    var myHeaders = new Headers();
+    myHeaders.append("Api-Key", "93fca97900d72d45cb1d0b24e47eea09c9fa5301158a53341e20bcd8bfed71cb");
+    myHeaders.append("Content-Type", "application/json");
+
+    this.requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(this.formData),
+      redirect: 'follow'
+    };
+
+    fetch("https://cloud-wallet-api.prod.affinity-project.org/api/v1/users/signup", this.requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log('signup--', result);
+        if (result['accessToken'] && result['did']) {
+          this.isAuthenticated = true;
+          this.accessToken = result['accessToken'];
+          this.did = result['did'];
+          this.unsigned();
+        } else {
+          this.login();
+        }
+      });
+  }
+
+  login() {
+    var myHeaders = new Headers();
+    myHeaders.append("Api-Key", "93fca97900d72d45cb1d0b24e47eea09c9fa5301158a53341e20bcd8bfed71cb");
+    myHeaders.append("Content-Type", "application/json");
+    this.formData = {
+      "username": (this.model['student']).replace(" ", "").replace(" ", ""),
+      "password": "Test@1234"
+    }
+    this.requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(this.formData),
+      redirect: 'follow'
+    };
+
+    fetch("https://cloud-wallet-api.prod.affinity-project.org/api/v1/users/login", this.requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log('login--', result);
+        console.log('login2--', result['accessToken'] && result['did']);
+        if (result['accessToken'] && result['did']) {
+          this.isAuthenticated = true;
+          this.accessToken = result['accessToken'];
+          this.did = result['did'];
+          this.unsigned();
+        }
+      });
+  }
+
+  unsigned() {
+    console.log('issueCertificate');
+    var myHeaders = new Headers();
+    myHeaders.append("Api-Key", "93fca97900d72d45cb1d0b24e47eea09c9fa5301158a53341e20bcd8bfed71cb");
+    myHeaders.append("Content-Type", "application/json");
+
+    this.formData = {
+      "type": "LeanEntityCredentialOrganizationV1",
+      "data": {
+        "@type": [
+          "Organization",
+          "OrganizationE",
+          "LeanEntityOrganization"
+        ],
+        "name": this.model['course'],
+        "hasCredential": [
+          {
+            "@type": [
+              "EducationalOccupationalCredential",
+              "Credential"
+            ],
+            "dateRevoked": this.model['date'],
+            "recognizedBy": {
+              "@type": "State",
+              "name": this.model['student']
+            }
+          },
+          {
+            "@type": [
+              "EducationalOccupationalCredential",
+              "Credential",
+              "OrganizationalCredential"
+            ],
+            "credentialCategory": this.model['type'],
+            "active": true,
+            "recognizedBy": {
+              "@type": "State",
+              "name": this.model['student']
+            }
+          }
+        ]
+      },
+      "holderDid": this.did
+    }
+
+    this.requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(this.formData),
+      redirect: 'follow'
+    };
+
+    fetch("https://affinity-issuer.prod.affinity-project.org/api/v1/vc/build-unsigned", this.requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log('unsigned--', result)
+        if (result['unsignedCredential']) {
+          this.sign(result['unsignedCredential'])
+        }
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  sign(unsinedData) {
+    var myHeaders = new Headers();
+    myHeaders.append("Api-Key", "93fca97900d72d45cb1d0b24e47eea09c9fa5301158a53341e20bcd8bfed71cb");
+    myHeaders.append("Authorization", "Bearer " + this.accessToken);
+    myHeaders.append("Content-Type", "application/json");
+
+    this.formData = {
+      "unsignedCredential": unsinedData,
+      "keyType": "rsa"
+    }
+    this.requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(this.formData),
+      redirect: 'follow'
+    };
+
+    fetch("https://cloud-wallet-api.prod.affinity-project.org/api/v1/wallet/sign-credential", this.requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log('sign--', result);
+        if (result['signedCredential']) {
+          this.signedCredential = result['signedCredential'];
+          this.store(result['signedCredential']);
+        }
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  store(sinedData) {
+    var myHeaders = new Headers();
+    myHeaders.append("Api-Key", "93fca97900d72d45cb1d0b24e47eea09c9fa5301158a53341e20bcd8bfed71cb");
+    myHeaders.append("Authorization", "Bearer " + this.accessToken);
+    myHeaders.append("Content-Type", "application/json");
+
+    this.formData = {
+      "data": [
+        sinedData
+      ]
+    }
+
+    this.requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(this.formData),
+      redirect: 'follow'
+    };
+
+    fetch("https://cloud-wallet-api.prod.affinity-project.org/api/v1/wallet/credentials", this.requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log('store-', result);
+        if(result['credentialIds']) {
+          this.share(result['credentialIds'][0])
+        }
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  share(claimId) {
+    var myHeaders = new Headers();
+    myHeaders.append("Api-Key", "93fca97900d72d45cb1d0b24e47eea09c9fa5301158a53341e20bcd8bfed71cb");
+    myHeaders.append("Authorization", "Bearer " + this.accessToken);
+
+    var raw = "";
+
+    this.requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("https://cloud-wallet-api.prod.affinity-project.org/api/v1/wallet/credentials/"+claimId+"/share", this.requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log('share-',result);
+        if(result['qrCode']){
+          this.generated = true;
+          this.qrCode = result['qrCode'];
+          var templateParams: any = {
+            name: 'James',
+            notes: 'Check this out!',
+            reply_to: 'paraspatel1434@gmail.com',
+            code:result['qrCode']
+        };
+        
+          emailjs.send('service_mihqxlf','template_heqihvf', templateParams, 'user_AB0BwpSVS4CHaQvPGkYsQ')
+          .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+          }, function(err) {
+            console.log('FAILED...', err);
+          });
+        }
+      })
+      .catch(error => console.log('error', error));
+  }
+
+
 }
