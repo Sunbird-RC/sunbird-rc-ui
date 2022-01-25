ac6e715fea83666a6b601a6f2d2cf1b40ec19ff4
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2022-01-25 19:08:48 +0530

    Task #178382 feat: Create Certificate template UI

--- src/app/safe-html.pipe.ts
+++ src/app/safe-html.pipe.ts
@@ -8,12 +8,11 @@ export class SafeHtmlPipe implements PipeTransform {
   constructor(protected sanitizer: DomSanitizer) {}
   public transform(value: any, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
     switch (type) {
-			case 'html': return this.sanitizer.bypassSecurityTrustHtml(value);
+			case 'html': return (value != undefined)? this.sanitizer.bypassSecurityTrustHtml(value) : '';
 			case 'style': return this.sanitizer.bypassSecurityTrustStyle(value);
 			case 'script': return this.sanitizer.bypassSecurityTrustScript(value);
 			case 'url': return this.sanitizer.bypassSecurityTrustUrl(value);
 			case 'resourceUrl': return this.sanitizer.bypassSecurityTrustResourceUrl(value);
-			default: throw new Error(`Invalid safe type specified: ${type}`);
 		}
   }
 
