c84a95d94adf6ce3f0e952fc13e85efb4f2097fd
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2022-01-25 15:51:32 +0530

    Task #178382 feat: Mulitiligual support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -255,36 +255,13 @@ import { FaqComponent } from './custom-components/faq/faq.component';
 
 export class AppModule {
 
-  languages = [];
   constructor(translate: TranslateService, authConfig: AuthConfigService) {
 
-    authConfig.getConfig().subscribe((config) => {
-      this.languages = config.hasOwnProperty('languages')? config.languages : [];
-      var installed_languages = [];
-
-      if(this.languages.length)
-      {
-        for (let i = 0; i < this.languages.length; i++) {
-          installed_languages.push({
-            "code": this.languages[i],
-            "name": ISO6391.getNativeName(this.languages[i])
-          });
-        }
-     
-      localStorage.setItem('languages', JSON.stringify(installed_languages));
-      translate.addLangs(this.languages);
+    if (localStorage.getItem('ELOCKER_LANGUAGE')) {
+      translate.use(localStorage.getItem('ELOCKER_LANGUAGE'));
+    } else {
+      translate.use('en');
     }
-
-      if (localStorage.getItem('setLanguage') && this.languages.includes(localStorage.getItem('setLanguage'))) {
-        translate.use(localStorage.getItem('setLanguage'));
-
-      } else {
-        const browserLang = translate.getBrowserLang();
-        let lang = this.languages.includes(browserLang) ? browserLang : 'en';
-        translate.use(lang);
-        localStorage.setItem('setLanguage', lang);
-      }
-    });
 
   }
 }
--- src/app/multilingual.init.ts
+++ src/app/multilingual.init.ts
@@ -7,9 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export function initLang(http: HttpClient, translate: TranslateService) {
   
   return () => new Promise<boolean>((resolve: (res: boolean) => void) => {
-    const defaultSetLanguage = 'en';
-    const localUrl = '/assets/i18n/local';
-    const globalUrl = '/assets/i18n/global';
+    const defaultELOCKER_LANGUAGE = 'en';
     const globalReqBody = {
       "entityType": [
         "Localization"
@@ -22,48 +20,47 @@ export function initLang(http: HttpClient, translate: TranslateService) {
       'Content-Type': 'application/json'
   }
 
-    const sufix = '.json';
-    const local = '-local';
-    const global = '-global';
-
-    const storageLocale = localStorage.getItem('setLanguage');
-    const setLanguage = storageLocale || defaultSetLanguage;
+    const storageLocale = localStorage.getItem('ELOCKER_LANGUAGE');
+    const ELOCKER_LANGUAGE = storageLocale || defaultELOCKER_LANGUAGE;
     var translatedKeys;
     var languageUrl;
 
  http.get('./assets/config/config.json').subscribe((res)=>{
    languageUrl = res['languageUrl'];
    console.log();
+   var installed_languages = [];
 
     forkJoin([
-      http.get(`${localUrl}/${setLanguage}${local}${sufix}`).pipe(
-        catchError(() => of(null))
-      ),
       http.post(languageUrl, globalReqBody, {headers : headers}).pipe(
         catchError(() => of(null))
-      ),
-      // http.get(`${globalUrl}/${setLanguage}${global}${sufix}`).pipe(
-      //   catchError(() => of(null))
-      // )
+      )
     ]).subscribe((response: any[]) => {
-      const devKeys = response[0];
-     // const translatedKeys = response[1];
+       translatedKeys = response[0];
 
-     if(response[1] != null && response[1].length){
-     for(let i = 0; i < response[1].length; i++){
-       console.log(response[1][i].language);
-       if(response[1][i].language == setLanguage)
+     if(response[0] != null && response[0].length){
+     for(let i = 0; i < response[0].length; i++){
+       console.log(response[0][i].language);
+       if(response[0][i].language == ELOCKER_LANGUAGE)
        {
-          translatedKeys = response[1][i].messages;
+          translatedKeys = response[0][i].messages;
        }
      }
+
+     for(let i = 0; i < response[0].length; i++){
+         installed_languages.push({
+           "code": response[0][i].language,
+           "name": (response[0][i].name).charAt(0).toUpperCase() + (response[0][i].name).slice(1).toLowerCase()
+         });
     }
 
-      translate.setTranslation(defaultSetLanguage, devKeys || {});
-      translate.setTranslation(setLanguage, translatedKeys || {}, true);
+     localStorage.setItem('languages', JSON.stringify(installed_languages));
 
-      translate.setDefaultLang(defaultSetLanguage);
-      translate.use(setLanguage);
+    }
+
+      translate.setTranslation(ELOCKER_LANGUAGE, translatedKeys || {}, true);
+
+      translate.setDefaultLang(defaultELOCKER_LANGUAGE);
+      translate.use(ELOCKER_LANGUAGE);
 
       resolve(true);
     });
