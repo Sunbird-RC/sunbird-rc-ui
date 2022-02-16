7618d18aa98e19b06674ef2d30a30c10a85a8583
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2022-02-16 18:46:47 +0530

    Task #1 feat: Theme Support

--- src/app/theme.config.ts
+++ src/app/theme.config.ts
@@ -1,27 +1,32 @@
 export const THEMES = {
 
   default: {
-    primaryColor: "#F07D00",
-    secondaryColor: "#4A227C",
+    primaryColor: "#1987B6",
+    secondaryColor: "#e8f3f8",
     bodyBackground: "#F6F8FC",
     cardBackground: "#FFFFFF",
     tagsBackground: "#F4ECFF",
-    navLabelColor: "#B275FF",
-    headerColor: "#4A227C",
-    headerLinkColor: "#F4ECFF",
+    navLabelColor: "#0098AB",
+    headerColor: "#FFFFFF", 
+    headerLinkColor: "#0098AB",
     primaryTextColor: "#000000",
-    secondaryTextColor: "#8D9091",
+    secondaryTextColor: "#000000",
+    linkColor: "#443DF6",
+    secondaryBtnBgColor: "#e8f3f8",
   },
   dark: {
-    primaryColor: "#F07D00",
-    secondaryColor: "#F07D00",
+    primaryColor: "#1987B6",
+    secondaryColor: "#1987B6",
     bodyBackground: "#080808",
     cardBackground: "#323232",
     tagsBackground: "#505050",
-    navLabelColor: "#FFB96C",
+    navLabelColor: "#0098AB",
     headerColor: "#313131",
     primaryTextColor: "#FFFFFF",
     secondaryTextColor: "#c3c3c3",
+    linkColor: "#443DF6",
+    secondaryBtnBgColor: "#e8f3f8",
+
   }
 };
 
